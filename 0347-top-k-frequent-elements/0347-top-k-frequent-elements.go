8d9191fa7dd104d2662aa23a9274f711d720a4b7
sreejith <166502113+sreejithkravi@users.noreply.github.com>
2025-02-18 17:44:40 +0530

    Time: 4 ms (22.16%), Space: 8.7 MB (5.68%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.go
@@ -0,0 +1,60 @@
+type MaxHeap struct {
+	arr []node
+}
+type node struct{
+    value int
+    freq int
+}
+func topKFrequent(nums []int, k int) []int {
+    h:=&MaxHeap{}
+    count:=make(map[int]int)
+    for _,val:=range nums{
+        count[val]++
+    }
+    for key,val:=range count{
+        h.arr=append(h.arr,node{key,val})
+    }
+    fmt.Println(h.arr)
+    h.build(h.arr)
+    fmt.Println(h.arr)
+    var topk[]int
+    for k!=0{
+        topk=append(topk,h.pop())
+        k--
+    }
+
+
+    return topk
+    
+}
+func (h *MaxHeap) pop() int {
+	max := h.arr[0].value
+	h.arr[0] = h.arr[len(h.arr)-1] // Move last element to root
+	h.arr = h.arr[:len(h.arr)-1]   // Remove last element
+	h.heapifyDown(0)
+
+    fmt.Println(max)
+	return max
+}
+func (h *MaxHeap) build(arr []node) {
+    for i:=len(h.arr)/2-1;i>=0;i--{
+        h.heapifyDown(i)
+    }
+}
+func (h *MaxHeap) heapifyDown(index int) {
+	largest := index
+	left := 2*index + 1
+	right := 2*index + 2
+
+	if left < len(h.arr) && h.arr[left].freq > h.arr[largest].freq  {
+		largest = left
+	}
+	if right < len(h.arr) && h.arr[right].freq > h.arr[largest].freq  {
+		largest = right
+	}
+
+	if largest != index {
+		h.arr[index], h.arr[largest] = h.arr[largest], h.arr[index]
+		h.heapifyDown(largest)
+	}
+}
