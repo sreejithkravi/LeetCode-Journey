18b1abad6a6e0555ad5fa5e3bbf7f8a090b15dd1
sreejith <166502113+sreejithkravi@users.noreply.github.com>
2025-05-12 16:33:53 +0530

    Time: 0 ms (100%), Space: 5.3 MB (74.67%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.go
@@ -0,0 +1,26 @@
+func findEvenNumbers(digits []int) []int {
+    var n1,n2,n3,flag int
+    var arr[]int
+    for i:=100;i<1000;i++{
+        n1=i%10
+        n2=(i/10)%10
+        n3=i/100
+        flag=0
+        for _,num:=range digits{
+            if num==n1 && n1!=-1{
+                flag++
+                n1=-1
+            }else if num==n2 && n2!=-1{
+                flag++
+                n2=-1
+            }else if num==n3 && n3!=-1{
+                flag++
+                n3=-1
+            }
+        }
+        if flag==3 && i%2==0{
+            arr=append(arr,i)
+        }
+    }
+    return arr
+} 
